8fa59691c3b9a6dff5682122a3e45cabb0f5b6ed
ha5750 <ckim135@jhu.edu>
2022-07-31 21:54:25 -0700

    initial commit

---
+++ worsereads-data-loader/src/main/java/my_project/worsereadsdataloader/WorsereadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package my_project.worsereadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorsereadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorsereadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ worsereads-data-loader/src/test/java/my_project/worsereadsdataloader/WorsereadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package my_project.worsereadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorsereadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
